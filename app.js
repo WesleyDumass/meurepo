3231c46bc5b77e2d86b006dad820f499064a6e3d
unknown <WESLEY_24212@ETEC.FURLAN>
2024-10-01 21:08:38 -0300

    2.0

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const sequelize = require('./config/config');
 const userRoutes = require ('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const setupSwagger = require('./swagger/swagger');
 
 app.use (express.json());
+setupSwagger(app);
 
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,7 @@ exports.listUsers = async (req, res) => {
     try{
         const users = await User.findAll(
             {
-                attributes: ['username','loginuser','active'], // 
+                attributes: ['id','username','loginuser','active'],
             });
             if(users.length === 0){
                 return res.status(404).json({message: 'User not found'});
@@ -12,6 +12,38 @@ exports.listUsers = async (req, res) => {
             res.json(users);
 
     } catch(error) {
+        res.status(500).send(error.message)
+    }
+};
+ 
+exports.activeUser = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findOne({where: {id: id}});
+        if(user){
+            user.active = true;
+            await user.save();
+            res.status(200).json({message: `User (${user.username}) activeted suscessful`});
+        } else{
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+};
+
+exports.deactivateUser = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findOne({where:{id: id}})
+        if(user){
+            user.active = false;
+            await user.save();
+            res.status(200).json({message: `User (${user.username}) deactiveted suscessfuly`});
+        } else{
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
         res.status(500).send(error.message)
     }
 };
